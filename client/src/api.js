9c44d2a9ca6d1c2bf5dbb5800b2274aea65f4159
Nathan Boothe <35839231+nathanboothe@users.noreply.github.com>
2026-07-21 17:55:50 -0400

    l

--- client/src/api.js
+++ client/src/api.js
@@ -9,7 +9,7 @@ async function request(path, options = {}) {
   });
   if (!res.ok) {
     const err = await res.json().catch(() => ({ error: res.statusText }));
-    'throw new Error(err.error || `HTTP ${res.status}`);
+    throw new Error(err.error || `HTTP ${res.status}`);
   }
   return res.json();
 }
